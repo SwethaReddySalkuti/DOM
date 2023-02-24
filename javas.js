5f7d9625160b058ddb80e6b559a353ad9f9c3117
SwethaReddySalkuti <salkutiswethareddy@gmail.com>
2023-02-24 18:33:59 +0530

    queryselectors commit

--- javas.js
+++ javas.js
@@ -1,2 +1,8 @@
-var li = document.getElementsByTagName('li');
-li[1].style.backgroundColor='red';
+//var li = document.getElementsByTagName('li');
+//li[1].style.backgroundColor='red';
+
+var input = document.querySelector('input[type="submit"]');
+input.value="SEND";
+
+var items = document.querySelectorAll('li');
+items[0].style.backgroundColor='yellow';
